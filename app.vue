6b4d70b21ca1339a2dd421c5df851cf31e6eac81
mm <467057463@qq.com>
2024-04-13 20:26:32 +0800

    init

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    {{ runtimeConfig.public.api }}
+    {{ appConfig.title }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+const runtimeConfig = useRuntimeConfig()
+console.log(runtimeConfig.public.api)
+
+const appConfig = useAppConfig();
+console.log(appConfig.title)
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  runtimeConfig: {
+    apiSercret: 123,
+    public: {
+      api: 'test'
+    }
+  }
+})
