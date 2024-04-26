d9162f9b56fdbe52e02f165547abb60699ad4def
mm <467057463@qq.com>
2024-04-26 18:04:28 +0800

    feat:
    1. 学习nuxt请求封装

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,20 @@
 <template>
   <div>index</div>
 </template>
+
+<script lang="ts" setup>
+import { getArticles } from '~/api/idnex'
+
+const res = await getArticles({page: 1, limit: 1})
+console.log('res', res)
+
+
+let token = useCookie('token',{
+  maxAge: 60 * 24 * 30
+})
+token = '123456' as any;
+console.log('token', token)
+
+const { data } = await useAsyncData('articles', () => getArticles({page: 1, limit: 1}))
+console.log('res', data)
+</script>
