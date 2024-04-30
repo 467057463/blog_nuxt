d65efe01a16eb9ba6547adeff826e24f10b9ae7c
mm <467057463@qq.com>
2024-04-30 19:39:53 +0800

    fix

--- ecosystem.stage.config.cjs
+++ ecosystem.stage.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'blog_nuxt-stage',
-      port: '3000',
+      port: '3001',
       exec_mode: 'cluster',
       instances: 'max',
       script: './.output/server/index.mjs'
