3e5b5f1974ae4f1f81f1b2fe7d4d847b8c09c3dc
mm <467057463@qq.com>
2024-04-30 14:44:58 +0800

    fix:
    1. 优化 stage 打包配置

--- ecosystem.config.stage.cjs
+++ ecosystem.config.stage.cjs
@@ -1,8 +1,8 @@
 module.exports = {
   apps: [
     {
-      name: 'blog_nuxt',
-      port: '3000',
+      name: 'blog_nuxt_stage',
+      port: '3001',
       exec_mode: 'cluster',
       instances: 'max',
       script: './.output/server/index.mjs'
