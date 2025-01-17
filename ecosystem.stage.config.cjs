a509407afa41840f31143ceb963696d4f8382515
mm <467057463@qq.com>
2025-01-17 16:41:01 +0800

    fix deploy

--- ecosystem.stage.config.cjs
+++ ecosystem.stage.config.cjs
@@ -6,9 +6,6 @@ module.exports = {
       exec_mode: 'cluster',
       instances: 'max',
       script: './.output/server/index.mjs',
-      env: {
-        DATABASE_URL: "mysql://root:Nandudu_0914@127.0.0.1:3306/blog-state_1"
-      }
     }
   ]
 }
