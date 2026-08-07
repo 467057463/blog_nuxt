e2b43999cf4c6acf2f38b6c147b98b19e8a9881d
maoyouwen <maoyouwen@nn.com>
2026-08-07 16:05:50 +0800

    fix

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,20 @@
 // import 'dotenv/config'
-import { defineConfig } from 'prisma/config'
+// import { defineConfig } from 'prisma/config'
 
 // Prisma 7 将 CLI 数据源与生成器解耦，避免 schema 和部署脚本各自维护连接地址。
-export default defineConfig({
+// export default defineConfig({
+//   schema: 'prisma/schema.prisma',
+//   migrations: {
+//     path: 'prisma/migrations',
+//     seed: 'tsx prisma/seed.ts',
+//   },
+//   datasource: {
+//     // url: env('DATABASE_URL'),
+//     url: 'mysql://root:nandudu_@127.0.0.1:3306/blog_development_1'
+//   },
+// })
+
+export default {
   schema: 'prisma/schema.prisma',
   migrations: {
     path: 'prisma/migrations',
@@ -12,4 +24,4 @@ export default defineConfig({
     // url: env('DATABASE_URL'),
     url: 'mysql://root:nandudu_@127.0.0.1:3306/blog_development_1'
   },
-})
+}
